74d38e53293fcc83e589524178583916525fcbdf
Richard Muir <richard.f.muir@gmail.com>
2017-07-27 12:00:34 +0100

    Add window.onload()

--- saveAsPDF.js
+++ saveAsPDF.js
@@ -3,3 +3,8 @@ btn.innerText = 'Download as PDF';
 btn.setAttribute("id", "downloadButton")
 btn.onclick = function(){window.print();};
 document.body.appendChild(btn);
+
+// Check when the onload function fires
+window.onload = function () { console.log('loaded'); 
+                              console.log(document.getElementById('pie')); // If this fires an error then the react page hasn't finished loading
+                            }
