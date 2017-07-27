0bf74b2ee80dc83e7779eeb19098e41705a6aa13
Richard Muir <richard.f.muir@gmail.com>
2017-07-27 11:53:39 +0100

    Add ID to button

--- saveAsPDF.js
+++ saveAsPDF.js
@@ -1,4 +1,5 @@
 let btn = document.createElement('button');
 btn.innerText = 'Download as PDF';
+btn.setAttribute("id", "downloadButton")
 btn.onclick = function(){window.print();};
 document.body.appendChild(btn);
