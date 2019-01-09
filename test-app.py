96e5902205c6ab2083fa33ed0273adaaf6f8478f
Richard Muir <richard.f.muir@gmail.com>
2019-01-09 22:31:03 +0000

    Follow a different tutorial

--- test-app.py
+++ test-app.py
@@ -1,3 +1,18 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Flask is running!'
+
+
+@app.route('/data')
+def names():
+    data = {"names": ["John", "Jacob", "Julie", "Jennifer"]}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
