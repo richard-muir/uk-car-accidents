5e1cb850fbc265c723619444fbb54980bdea492a
Richard Muir <richard.f.muir@gmail.com>
2019-01-09 22:17:38 +0000

    Simplify test-app

--- test-app.py
+++ test-app.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["Hello!"]
